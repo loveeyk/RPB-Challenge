4fa60c268b5a862631e1a26dd38167d64bd5767e
Loveleen Karir <karir.loveleen@gmail.com>
2021-10-22 20:21:23 +0200

    feat: add description, improve styling

--- rpb-teaser/src/components/Slider.vue
+++ rpb-teaser/src/components/Slider.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="slider">
-    <template class="slider__wrapper" v-for="(option, i) in traveloptions">
-      <img
-        :src="option.image"
-        :alt="`slider-image-${i}`"
-        :id="`slider-image-${i}`"
-        :key="i"
-      />
-    </template>
+    <div class="slider__wrapper" v-for="(option, i) in traveloptions" :key="i">
+      <a :href="option.link" class="slider__link">
+        <img
+          :src="option.image"
+          :alt="`slider-image-${i}`"
+          :id="`slider-image-${i}`"
+        />
+        <div class="slider__description">
+          <span class="slider__text">{{ option.text }}</span>
+          <span class="slider__price">ab {{ option.price }}</span>
+        </div></a
+      >
+    </div>
   </div>
 </template>
 
@@ -27,11 +32,44 @@ export default class Slider extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import "src/assets/global.scss";
+@import "src/assets/colors.scss";
 .slider {
   display: flex;
+  overflow-y: hidden;
+
+  &__link {
+    text-decoration: none;
+  }
 
   &__wrapper {
-    border: 3px solid black;
+    border: 1px solid $border-color;
+    box-shadow: -1pt 1pt 0 0 $border-color;
+    border-radius: 10px;
+    margin: 1rem;
+
+    img {
+      border: 1px solid $border-color;
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+    }
+  }
+
+  &__description {
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem;
+  }
+  &__text {
+    color: $text-color1;
+    font-family: Open Sans;
+    font-weight: bold;
+  }
+
+  &__price {
+    color: $text-color3;
+    font-family: Open Sans;
+    font-weight: bold;
   }
 }
 </style>
