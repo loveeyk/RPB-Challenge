aec2a4fc1aa0954df4b32374e884818736458b67
Loveleen Karir <karir.loveleen@gmail.com>
2021-10-22 20:20:24 +0200

    refactor: change Component name

--- rpb-teaser/src/components/CountryRegionTeaser.vue
+++ rpb-teaser/src/components/CountryRegionTeaser.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="teaser">
     <div class="teaser__heading">
-      <h1>Länder, Regionen, Orte</h1>
-      <span>Alle</span>
+      <h1 class="heading">Länder, Regionen, Orte</h1>
+      <router-link to=/reiseziele class="link">Alle</router-link>
     </div>
 
-    <h2>Lorem Ipsum Länder, Regionen, Orte</h2>
+    <h2 class="teaser__subheading">Lorem Ipsum Länder, Regionen, Orte</h2>
     <div class="teaser__slider">
       <Slider :traveloptions="pictures" />
     </div>
@@ -22,7 +22,7 @@ import Slider from "./Slider.vue";
     Slider,
   },
 })
-export default class Teaser extends Vue {
+export default class CountryRegionTeaser extends Vue {
   pictures: { text: string; price: number; image: string; link: string }[] = [
     ...traveloptions,
   ];
@@ -31,10 +31,33 @@ export default class Teaser extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import "src/assets/global.scss";
+@import "src/assets/colors.scss";
+
 .teaser {
   &__heading {
     display: flex;
     align-items: baseline;
+
+    .heading{
+      font-family: Quicksand;
+      font-weight: bold;
+      color: $text-color1;
+
+    }
+
+    .link{
+      text-decoration: none;
+      font-family: Open Sans;
+      color: $text-color1;
+      text-align: right;
+      margin-left: 10%;
+    }
   }
+
+  &__subheading{
+      color: $text-color2;
+      font-family: Open Sans;
+    }
 }
 </style>
