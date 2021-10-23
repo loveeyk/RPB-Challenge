a34f6b38bcd13bb43452e7f9489917c8b9cc8171
Loveleen Karir <karir.loveleen@gmail.com>
2021-10-23 15:41:20 +0200

    feat: add slider functionality

--- rpb-teaser/src/components/Slider.vue
+++ rpb-teaser/src/components/Slider.vue
@@ -1,23 +1,37 @@
 <template>
-  <div class="slider">
-    <div class="slider__wrapper" v-for="(option, i) in traveloptions" :key="i">
-      <a :href="option.link" class="slider__link">
-        <img
-          :src="option.image"
-          :alt="`slider-image-${i}`"
-          :id="`slider-image-${i}`"
-        />
-        <div class="slider__description">
-          <span class="slider__text">{{ option.text }}</span>
-          <span class="slider__price">ab {{ option.price }}</span>
-        </div></a
+  <div>
+    <div class="slider">
+      <div
+        class="slider__wrapper"
+        v-for="(option, i) in pictures"
+        :key="i"
+        v-bind="traveloptions"
       >
+        <a :href="option.link" class="slider__link">
+          <img
+            :src="option.image"
+            :alt="`slider-image-${i}`"
+            :id="`slider-image-${i}`"
+          />
+          <div class="slider__description">
+            <span class="slider__text">{{ option.text }}</span>
+            <span class="slider__price">ab {{ option.price }}</span>
+          </div></a
+        >
+      </div>
     </div>
+    <button @click="left" class="button button-left">
+      <img src="../assets/img/left.svg" alt="left" />
+    </button>
+    <button @click="right" class="button button-right">
+      <img src="../assets/img/right.svg" alt="right" />
+    </button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
+import { traveloptions } from "../travel-options";
 
 @Component
 export default class Slider extends Vue {
@@ -27,6 +41,19 @@ export default class Slider extends Vue {
     image: string;
     link: string;
   }[];
+
+  pictures = [...traveloptions];
+
+  left = () => {
+    let tmp = this.pictures.shift();
+    this.pictures.push(tmp);
+    this.pictures.forEach((option) => console.log(option.text));
+    this.$forceUpdate();
+  };
+  right = () => {
+    let tmp = this.pictures.pop();
+    this.pictures.unshift(tmp);
+  };
 }
 </script>
 
@@ -83,6 +110,20 @@ export default class Slider extends Vue {
     color: $text-color3;
     font-family: Open Sans;
     font-weight: bold;
+  }
+}
+.button {
+  position: absolute;
+  top: 50%;
+  z-index: 10;
+  background: transparent;
+  border-radius: 5px;
+  border: 1px solid transparent;
+  &-left {
+    left: 0;
+  }
+  &-right {
+    right: 0;
   }
 }
 </style>
