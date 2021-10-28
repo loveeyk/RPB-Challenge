1769c360d34c96260ab82446593bde4462201236
Loveleen Karir <karir.loveleen@gmail.com>
2021-10-29 01:57:32 +0200

    chore

--- rpb-teaser/src/components/CountryRegionTeaser.vue
+++ rpb-teaser/src/components/CountryRegionTeaser.vue
@@ -1,33 +1,32 @@
-<template><div>
+<template>
+<div>
   <div class="teaser">
     <div class="teaser__heading">
       <h1 class="heading">Länder, Regionen, Orte</h1>
       <router-link to=/reiseziele class="link">Alle</router-link>
     </div>
 
-    <h2 class="teaser__subheading">Lorem Ipsum Länder, Regionen, Orte</h2>
+    <p class="teaser__subheading">Lorem Ipsum Länder, Regionen, Orte</p>
     
   </div>
   <div class="teaser__slider">
-      <Slider :traveloptions="pictures" />
-    </div></div>
+      <Slider />
+    </div>
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import { traveloptions } from "../travel-options";
+import { Component, Vue} from "vue-property-decorator";
 import Slider from "./Slider.vue";
 
 @Component({
   components: {
     Slider,
+    
   },
 })
 export default class CountryRegionTeaser extends Vue {
-  pictures: { text: string; price: number; image: string; link: string }[] = [
-    ...traveloptions,
-  ];
-}
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -38,6 +37,7 @@ export default class CountryRegionTeaser extends Vue {
 .teaser {
   padding-left: 23px;
   margin-bottom: 25px;
+  padding: 1rem 1.4rem;
 
   &__heading {
     display: flex;
@@ -45,9 +45,8 @@ export default class CountryRegionTeaser extends Vue {
 
     .heading{
       font-family: Quicksand;
-      font-weight: bolder;
       color: $text-color1;
-      font-size: 24px;
+      font-size: 1.5rem;
 
     }
 
@@ -57,27 +56,30 @@ export default class CountryRegionTeaser extends Vue {
       color: $text-color1;
       text-align: right;
       margin-left: 10%;
-      font-size: 16px;
+      font-size: 1rem;
     }
   }
   &__slider::before {
-        content: "";
-    position: absolute;
+    content: "";
+    position: fixed;
     background: linear-gradient(273deg, #ffffff, #ffffff00);
     height: 222px;
     right: 0px;
     z-index: 1;
     width: 23px;
+    top: 28%;
   }
     &__slider{
+      padding-left: 1.4rem;
       position: relative;
-      padding-left: 23px;
+      overflow-y: hidden;
     }
+     
 
   &__subheading{
       color: $text-color2;
-      font-family: Open Sans;
-      font-size: 16px;
+      font-family: 'Open Sans';
+      font-size: 1rem;
     }
 }
 </style>
